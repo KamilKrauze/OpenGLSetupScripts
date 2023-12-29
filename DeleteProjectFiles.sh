e7419df2b1bf24cadf2d96b1798835f4efa2a460
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2023-12-29 16:31:33 +0000

    Update DeleteProjectFiles.sh

--- DeleteProjectFiles.sh
+++ DeleteProjectFiles.sh
@@ -2,24 +2,6 @@
 
 echo "Deleting all project files"
 
-extensions=".sln .vcxproj .vcxproj.filters .vcxproj.users"
-
-if [ -d "./build" ]; then
-    echo "Deleting directory: './build'"
-    rm -r "./build"
-fi
-
-for d in */; do
-    echo "Processing directory: $d"
-    cd "$d" || exit
-    for e in $extensions; do
-        if ls *"$e" 1>/dev/null 2>&1; then
-            rm -f *"$e"
-        fi
-    done
-    cd ..
-done
-
-rm ./*.sln
+find . -maxdepth 1 -type f -name 'Makefile' -delete
 
 echo "Complete!"
