512632f089f9f7fd330f86b7cad4e53b016581cc
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2023-12-29 16:29:57 +0000

    Update GenerateProjectFiles.bat

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,14 +1,25 @@
-call premake5.exe vs2022
-
 @echo off
-if not exist "./include" (
-  mkdir include
+
+IF NOT "-d" "%CD%\thirdparty\include" (
+  echo "Creating 'include' directory..."
+  echo
+  cd "%CD%\thirdparty"
+  mkdir "include"
+  mkdir "%CD%\include\glad"
+  mkdir "%CD%\include\KHR"
+  COPY  "%CD%\glm\glm" "%CD%\include\glm"
+  COPY  "%CD%\glfw\include\GLFW" "./include/"
+  COPY  "./stb/" "%CD%\include\stb"
+  wget "-O" "%CD%\include\glad\glad.h" "https://glad.dav1d.de/generated/tmpnbaj81ksglad/include/glad/glad.h"
+  wget "-O" "%CD%\..\src\glad.c" "https://glad.dav1d.de/generated/tmpnbaj81ksglad/src/glad.c"
+  wget "-O" "%CD%\include\KHR\khrplatform.h" "https://glad.dav1d.de/generated/tmpnbaj81ksglad/include/KHR/khrplatform.h"
+  cd ".."
 )
-
-if not exist "./lib" (
-  mkdir lib
+IF NOT "-d" "%CD%\thirdparty\lib" (
+  echo "Creating 'lib' directory..."
+  cd "%CD%\thirdparty"
+  mkdir "lib"
+  cd ".."
 )
-
-@echo on
-
-pause
+./premake5 vs2022
+echo "Done..."
